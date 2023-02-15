14cc4304b9b0f8436e384618f2f5dab48ccca2da
lifangjun <lifj@ebondhm.com>
2023-02-15 13:49:42 +0800

    feat(file IO):文件操作

---
+++ 02_day/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+// ClearAndWrite 文件读写
+func ClearAndWrite() {
+	filePath := "/home/lifangjun/Desktop/go_study/learn_go/02_day/1.txt"
+	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Printf("open file err = %v\n", err)
+		return
+	}
+	//　close file stream
+	defer file.Close()
+	// write
+
+	wStr := "您好！！今晚我想吃鱼了."
+	writer := bufio.NewWriter(file)
+	for i := 0; i < 10; i++ {
+		writer.WriteString(wStr)
+	}
+	// 因为　writer 是带缓存的，需要通过 flush 到磁盘
+	writer.Flush()
+	file.Seek(0, 0)
+
+	// read
+	reader := bufio.NewReader(file)
+	for {
+		str, err := reader.ReadString('.')
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(str)
+	}
+}
+
+// CopyFileToNewFile 拷贝文件内容到新的文件中
+func CopyFileToNewFile() {
+	oldFilePath := "/home/lifangjun/Desktop/go_study/learn_go/02_day/1.txt"
+	newFilePath := "/home/lifangjun/Desktop/go_study/learn_go/02_day/2.txt"
+	data, err := ioutil.ReadFile(oldFilePath)
+	if err != nil {
+		fmt.Printf("read file err = %v", err)
+	}
+	err = ioutil.WriteFile(newFilePath, data, 0666)
+	if err != nil {
+		fmt.Printf("write file err=%v", err)
+	}
+}
+
+// JudgePathIsExists 判断文件或者文件夹是否存在
+func JudgePathIsExists() (bool, error) {
+	myPath := "/home/lifangjun/Desktop/go_study/learn_go/02_day/1.txt"
+	_, err := os.Stat(myPath)
+	if err == nil {
+		fmt.Printf("当前文件/文件夹存在\n")
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		fmt.Printf("当前文件/文件夹不存在\n")
+		return false, nil
+	}
+	return false, nil
+}
+
+// FileCopy 文件拷贝
+func FileCopy() {
+
+}
+
+func main() {
+	// ClearAndWrite()
+	//CopyFileToNewFile()
+	//JudgePathIsExists()
+}
